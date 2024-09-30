396e38cc3bdd0ecfc304600533ad6b51db05fb73
Firdaous02 <129233209+Firdaous02@users.noreply.github.com>
2024-09-30 21:21:17 +0100

    Update callbacks.py

--- callbacks.py
+++ callbacks.py
@@ -16,34 +16,6 @@ from sqlalchemy import text
 import base64
 import os
 
-# def register_callbacks(dash_app):
-#     @dash_app.callback(
-#         [Output('login-message', 'children'),
-#          Output('url', 'pathname')],
-#         [Input('login-button', 'n_clicks')],
-#         [State('email-input', 'value'), State('password-input', 'value')]
-#     )
-#     def verify_login(n_clicks, email, password):
-#         if n_clicks is None:
-#             return "", no_update
-
-#         if not email or not password:
-#             return html.Div(
-#                 "Please enter both email and password.",
-#                 className='alert alert-danger'
-#             ), no_update
-
-#         # Appeler la fonction de vérification depuis le fichier db.py
-#         user = verify_user(email, password)
-#         if user:
-#             session['authenticated'] = True  # Stocker l'état d'authentification dans la session
-#             return "Redirecting...", "/dashboard"
-#         else:
-#             return html.Div(
-#                 f'Invalid email or password. Please try again.',
-#                 className='alert alert-danger'
-#             ), no_update
-
 
 def dashboard_callbacks(dash_app):
     @dash_app.callback(
